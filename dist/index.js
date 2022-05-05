970a1c2adb188b5082461c5714fbebd368243c7e
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-05-04 20:44:17 -0400

    reorganize for gh-pages

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "../app/App";
 import { Provider } from "react-redux";
-import store from "./store/index";
+import store from "../app/store/index";
 const container = document.getElementById("app");
 const root = createRoot(container);
 
