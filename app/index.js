3f57f292426f55c48cccebabd5c9e8b01403a8cf
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-05-18 23:56:03 -0400

    add css and style loaders

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/index";
+import "./style.css";
 const container = document.getElementById("app");
 const root = createRoot(container);
 
